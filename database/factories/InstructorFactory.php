fa71c547c0b200c5061145305280b494b05b30f5
TNORTHERN <traynorthern@yahoo.com>
2019-09-21 03:57:23 -0500

    Instructor Base Factory

--- database/factories/InstructorFactory.php
+++ database/factories/InstructorFactory.php
@@ -7,6 +7,13 @@ use Faker\Generator as Faker;
 
 $factory->define(Instructor::class, function (Faker $faker) {
     return [
-        //
+        'InstructorFirstName' => $faker->firstName,
+        'InstructorLastName' => $faker->lastName,
+        'InstructorEmail' => $faker->email,
+        'InstructorPhone' => $faker->phoneNumber,
+        'DegreeEarned' => $faker->jobTitle,
+        'DegreeSchool' => $faker->lastName,
+        'Description' => $faker->text,
+        'Admin' => 1,
     ];
 });
