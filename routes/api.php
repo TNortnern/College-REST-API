edb4baa9f2e88ce644b7f109e222132043324b83
TNORTHERN <traynorthern@yahoo.com>
2019-09-21 04:21:18 -0500

    Changing all routes to API endpoints

--- routes/api.php
+++ routes/api.php
@@ -29,4 +29,41 @@ Route::get('students/', 'StudentController@index');
 
   Route::get('programs/', 'ProgramController@index');
 
+  Route::resource('students', 'StudentController', [
+    'except' => 'index'
+]);
+
+Route::resource('instructors', 'InstructorController', [
+    'except' => 'index'
+]);
+
+Route::resource('courses', 'CourseController', [
+    'except' => 'index'
+]);
+
+Route::post("/checklogin", 'UserController@checklogin');
+Route::post('checkadmin', 'UserController@checkIfAdmin');
+
+ Route::post('courses/', 'CourseController@store');
+
+ Route::post('getTaughtCourses', 'InstructorController@getTaughtCourses');
+ Route::post('getStudentsInEachCourse', 'InstructorController@getStudentsInEachCourse');
+
+Route::get('/home', 'HomeController@index')->name('home');
+Route::post('enroll', 'CourseController@enroll');
+Route::post('checkenrolled', 'CourseController@checkEnrolled');
+Route::post('dropcourse', 'CourseController@dropCourse');
+Route::post('courses/search', 'CourseController@search');
+Route::post('getcourse', 'CourseController@getCourse');
+Route::post('searchByName', 'CourseController@searchByName');
+
+Route::post('/register', 'Auth\RegisterController@make');
+Route::post('/signin', 'Auth\LoginController@signin');
+Route::post('/signout', 'Auth\LoginController@signout');
+// Auth::routes();
+
+Route::post('/getuser', 'UserController@getUserInfo');
+Route::post('/getusercourses', 'UserController@getUserCourses');
+
+
 
--- routes/web.php
+++ routes/web.php
@@ -15,38 +15,38 @@ Route::get('/{vue_capture?}', function() {
    return view('welcome');
 })->where('vue_capture', '[\/\w\.-]*');
 
-Route::resource('students', 'StudentController', [
-    'except' => 'index'
-]);
+// Route::resource('students', 'StudentController', [
+//     'except' => 'index'
+// ]);
 
-Route::resource('instructors', 'InstructorController', [
-    'except' => 'index'
-]);
+// Route::resource('instructors', 'InstructorController', [
+//     'except' => 'index'
+// ]);
 
-Route::resource('courses', 'CourseController', [
-    'except' => 'index'
-]);
+// Route::resource('courses', 'CourseController', [
+//     'except' => 'index'
+// ]);
 
-Route::post("/checklogin", 'UserController@checklogin');
-Route::post('checkadmin', 'UserController@checkIfAdmin');
+// Route::post("/checklogin", 'UserController@checklogin');
+// Route::post('checkadmin', 'UserController@checkIfAdmin');
 
- Route::post('courses/', 'CourseController@store');
+//  Route::post('courses/', 'CourseController@store');
 
- Route::post('getTaughtCourses', 'InstructorController@getTaughtCourses');
- Route::post('getStudentsInEachCourse', 'InstructorController@getStudentsInEachCourse');
+//  Route::post('getTaughtCourses', 'InstructorController@getTaughtCourses');
+//  Route::post('getStudentsInEachCourse', 'InstructorController@getStudentsInEachCourse');
 
-Route::get('/home', 'HomeController@index')->name('home');
-Route::post('enroll', 'CourseController@enroll');
-Route::post('checkenrolled', 'CourseController@checkEnrolled');
-Route::post('dropcourse', 'CourseController@dropCourse');
-Route::post('courses/search', 'CourseController@search');
-Route::post('getcourse', 'CourseController@getCourse');
-Route::post('searchByName', 'CourseController@searchByName');
+// Route::get('/home', 'HomeController@index')->name('home');
+// Route::post('enroll', 'CourseController@enroll');
+// Route::post('checkenrolled', 'CourseController@checkEnrolled');
+// Route::post('dropcourse', 'CourseController@dropCourse');
+// Route::post('courses/search', 'CourseController@search');
+// Route::post('getcourse', 'CourseController@getCourse');
+// Route::post('searchByName', 'CourseController@searchByName');
 
-Route::post('/register', 'Auth\RegisterController@make');
-Route::post('/signin', 'Auth\LoginController@signin');
-Route::post('/signout', 'Auth\LoginController@signout');
-// Auth::routes();
+// Route::post('/register', 'Auth\RegisterController@make');
+// Route::post('/signin', 'Auth\LoginController@signin');
+// Route::post('/signout', 'Auth\LoginController@signout');
+// // Auth::routes();
 
-Route::post('/getuser', 'UserController@getUserInfo');
-Route::post('/getusercourses', 'UserController@getUserCourses');
+// Route::post('/getuser', 'UserController@getUserInfo');
+// Route::post('/getusercourses', 'UserController@getUserCourses');
